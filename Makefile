63d19e71062ba3a11a9ab9f259a97da294ddee9c
Abraten <anton@ju60.de>
2022-05-09 13:08:23 +0200

    improve navigation code

---
+++ Makefile
@@ -0,0 +1,12 @@
+.PHONY: all simulator navigation
+all: get build install
+
+sim: ## Launch turtlebot simulator
+	export TURTLEBOT3_MODEL="burger"
+	roslaunch turtlebot3_fake turtlebot3_fake.launch
+
+nav: ## Launch navigation package
+	rosrun ball_schubser_navigation control.py
+
+help: ## Display this help screen
+	@grep -h -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
--- navigation/src/control.py
+++ navigation/src/control.py
@@ -5,15 +5,18 @@ import rospy
 from geometry_msgs.msg import Twist
 
 def talker():
-        # create publisher called cmd_vel
+    # create publisher called cmd_vel
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-        # pobably different node name
+    # pobably different node name
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-                # type for cmd_vel is Twist (has linear for velocity and angular for steering)
+        # type for cmd_vel is Twist (has linear for velocity and angular for steering)
         twist = Twist()
-        twist.linear.x = 0.4
+        # 1.0 and -1.0
+        twist.linear.x = 0.3
+        # steering between 3.0 and -3.0
+        twist.angular.z = -1.5
         pub.publish(twist)
         rate.sleep()
 
