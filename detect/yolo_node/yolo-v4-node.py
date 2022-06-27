3e0987143224b61abaf0978ac65c09448e1ba88a
RunRanger <jan.e.mayer@web.de>
2022-06-27 11:47:08 +0200

    +added distance calculation

--- detect/yolo_node/yolo-v4-node.py
+++ detect/yolo_node/yolo-v4-node.py
@@ -1,9 +1,11 @@
+from detect.yolo_node.distanceCalculation import get_distance_of_ball
 import rospy
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Quaternion
 from cv_bridge import CvBridge
 import cv2
 from yolo_minimal import MinimalYolov4
+
 
 class YoloNode(object):
     def __init__(self):
@@ -12,7 +14,8 @@ class YoloNode(object):
         self.loop_rate = rospy.Rate(10)
         self.TARGET_OBJECT = 'sports ball'
         self.DESTINATION_OBJECT = 'bottle'
-        self.model = MinimalYolov4(weight_path='weights/yolov4.weights', class_name_path='class_names/coco_classes.txt')
+        self.model = MinimalYolov4(
+            weight_path='weights/yolov4.weights', class_name_path='class_names/coco_classes.txt')
 
         # Publishers
         self.pub = rospy.Publisher('ball_pos', Quaternion, queue_size=5)
@@ -45,21 +48,30 @@ class YoloNode(object):
                 msg.w = -1.0
 
                 if not prediction.empty:
-                    targets = prediction.loc[prediction['class_name'] == self.TARGET_OBJECT]
-                    destinations = prediction.loc[prediction['class_name'] == self.DESTINATION_OBJECT]
+                    targets = prediction.loc[prediction['class_name']
+                                             == self.TARGET_OBJECT]
+                    destinations = prediction.loc[prediction['class_name']
+                                                  == self.DESTINATION_OBJECT]
 
                     if not targets.empty:
-                        sorted_lowest = targets.sort_values('y1', ascending=True)
+                        sorted_lowest = targets.sort_values(
+                            'y1', ascending=True)
 
                         nearest_target = sorted_lowest.iloc[0]
                         x1 = nearest_target['x1']
                         w = nearest_target['w']
+                        h = nearest_target['h']
                         x_center = x1 + 0.5 * w
 
                         y2 = nearest_target['y2']
 
                         msg.x = x_center / self.image.shape[1]
                         msg.y = y2 / self.image.shape[0]
+
+                        pos = get_distance_of_ball(
+                            w, h, x1, self.image.shape[1], self.image.shape[0])
+                        msg.x = pos.x
+                        msg.y = pos.y
 
                     if not destinations.empty:
 
@@ -68,15 +80,23 @@ class YoloNode(object):
                         destination = destinations.iloc[0]
                         x1 = destination['x1']
                         w = destination['w']
+                        h = nearest_target['h']
                         x_center = x1 + 0.5 * w
 
                         y2 = destination['y2']
 
                         msg.z = x_center / self.image.shape[1]
                         msg.w = y2 / self.image.shape[0]
-            
+
+                        pos = get_distance_of_ball(
+                            w, h, x1, self.image.shape[1], self.image.shape[0])
+
+                        msg.z = pos.x
+                        msg.w = pos.y
+
                 self.pub.publish(msg)
             self.loop_rate.sleep()
+
 
 if __name__ == '__main__':
     rospy.init_node('target_calculation')
